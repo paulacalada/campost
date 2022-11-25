69433ae37b1a1979b53f11941a0e3e8113ae0aa7
haisenheim <clementessomba@gmail.com>
2022-11-25 20:05:55 +0100

    lundi soir

--- campost/client/forms.py
+++ campost/client/forms.py
@@ -39,8 +39,10 @@ class CompteGestion(forms.ModelForm):
         }
         
 class Login(forms.Form):
-    username = forms.CharField(label="Nom d'utilisateur", max_length=30,attrs={'class': 'form-control'})
-    password = forms.CharField(label="Mot de passe", widget=forms.PasswordInput,attrs={'class': 'form-control'})
+    username = forms.CharField(label="Nom d'utilisateur", max_length=100,
+                          widget=forms.TextInput(
+                              attrs={'class': "form-control"}))
+    password = forms.CharField(label="Mot de passe", widget=forms.PasswordInput(attrs={'class': "form-control"}))
     
 class ClientSearch(forms.Form):
     numero = forms.CharField(label="Numero de compte", max_length=100)
